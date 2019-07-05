098aa14c5c8237d89733e1f505bec70d2fece8ee
wei <w21180239@163.com>
2019-07-05 14:10:09 +0800

    DGL

--- main.py
+++ main.py
@@ -1,30 +1,167 @@
-import torch
-import torch.nn.functional as F
-import networkx as nx
+import argparse, time, math
 import numpy as np
+import torch
+import torch.nn as nn
+import torch.nn.functional as F
+import dgl
+import dgl.function as fn
+from dgl import DGLGraph
+from dgl.data import register_data_args, load_data
+import networkx as nx
 import os
 import pickle
 import pandas as pd
-from torch_geometric.data import NeighborSampler,Data
-from torch_geometric.nn import SAGEConv, GATConv
-from torch_geometric.utils import remove_self_loops
 from sklearn.preprocessing import Imputer, RobustScaler,StandardScaler
+
 def load_obj(name ):
     with open( name + '.pkl', 'rb') as f:
         return pickle.load(f)
-if os.path.exists('data.npz'):
-    data = torch.load('data.npz')
-else:
-    G = nx.read_gpickle('my_graph.gpickle')
+class NodeUpdate(nn.Module):
+    def __init__(self, layer_id, in_feats, out_feats, dropout, activation=None, test=False, concat=False):
+        super(NodeUpdate, self).__init__()
+        self.layer_id = layer_id
+        self.linear = nn.Linear(in_feats, out_feats)
+        self.dropout = None
+        if dropout != 0:
+            self.dropout = nn.Dropout(p=dropout)
+        self.activation = activation
+        self.concat = concat
+        self.test = test
+
+    def forward(self, node):
+        h = node.data['h']
+        if self.test:
+            norm = node.data['norm']
+            h = h * norm
+        else:
+            agg_history_str = 'agg_h_{}'.format(self.layer_id-1)
+            agg_history = node.data[agg_history_str]
+            # control variate
+            h = h + agg_history
+            if self.dropout:
+                h = self.dropout(h)
+        h = self.linear(h)
+        if self.concat:
+            h = torch.cat((h, self.activation(h)), dim=1)
+        elif self.activation:
+            h = self.activation(h)
+        return {'activation': h}
+
+
+class GCNSampling(nn.Module):
+    def __init__(self,
+                 in_feats,
+                 n_hidden,
+                 n_classes,
+                 n_layers,
+                 activation,
+                 dropout):
+        super(GCNSampling, self).__init__()
+        self.n_layers = n_layers
+        self.dropout = None
+        if dropout != 0:
+            self.dropout = nn.Dropout(p=dropout)
+        self.activation = activation
+        # input layer
+        self.linear = nn.Linear(in_feats, n_hidden)
+        self.layers = nn.ModuleList()
+        # hidden layers
+        for i in range(1, n_layers):
+            skip_start = (i == n_layers-1)
+            self.layers.append(NodeUpdate(i, n_hidden, n_hidden, dropout, activation, concat=skip_start))
+        # output layer
+        self.layers.append(NodeUpdate(n_layers, 2*n_hidden, n_classes, dropout))
+
+    def forward(self, nf):
+        h = nf.layers[0].data['preprocess']
+        if self.dropout:
+            h = self.dropout(h)
+        h = self.linear(h)
+
+        skip_start = (0 == self.n_layers-1)
+        if skip_start:
+            h = torch.cat((h, self.activation(h)), dim=1)
+        else:
+            h = self.activation(h)
+
+        for i, layer in enumerate(self.layers):
+            new_history = h.clone().detach()
+            history_str = 'h_{}'.format(i)
+            history = nf.layers[i].data[history_str]
+            h = h - history
+
+            nf.layers[i].data['h'] = h
+            nf.block_compute(i,
+                             fn.copy_src(src='h', out='m'),
+                             lambda node : {'h': node.mailbox['m'].mean(dim=1)},
+                             layer)
+            h = nf.layers[i+1].data.pop('activation')
+            # update history
+            if i < nf.num_layers-1:
+                nf.layers[i].data[history_str] = new_history
+
+        return h
+
+
+class GCNInfer(nn.Module):
+    def __init__(self,
+                 in_feats,
+                 n_hidden,
+                 n_classes,
+                 n_layers,
+                 activation):
+        super(GCNInfer, self).__init__()
+        self.n_layers = n_layers
+        self.activation = activation
+        # input layer
+        self.linear = nn.Linear(in_feats, n_hidden)
+        self.layers = nn.ModuleList()
+        # hidden layers
+        for i in range(1, n_layers):
+            skip_start = (i == n_layers-1)
+            self.layers.append(NodeUpdate(i, n_hidden, n_hidden, 0, activation, True, concat=skip_start))
+        # output layer
+        self.layers.append(NodeUpdate(n_layers, 2*n_hidden, n_classes, 0, None, True))
+
+    def forward(self, nf):
+        h = nf.layers[0].data['preprocess']
+        h = self.linear(h)
+
+        skip_start = (0 == self.n_layers-1)
+        if skip_start:
+            h = torch.cat((h, self.activation(h)), dim=1)
+        else:
+            h = self.activation(h)
+
+        for i, layer in enumerate(self.layers):
+            nf.layers[i].data['h'] = h
+            nf.block_compute(i,
+                             fn.copy_src(src='h', out='m'),
+                             fn.sum(msg='m', out='h'),
+                             layer)
+            h = nf.layers[i+1].data.pop('activation')
+
+        return h
+
+
+def main(args):
+    # load and preprocess dataset
+    # data = load_data(args)
+
     if os.path.exists('sur_x.npy'):
         x = np.load('sur_x.npy')
         y = np.load('sur_y.npy')
+        y = y.reshape(-1, 1)
+        y = StandardScaler().fit_transform(y)
+        x = Imputer().fit_transform(x)
+        x = RobustScaler().fit_transform(x)
         data_df = pd.DataFrame(pd.read_csv('new_playerCharge-4.csv'))
         id_list = np.array(data_df['openid'])
     else:
         data_df = pd.DataFrame(pd.read_csv('new_playerCharge-4.csv'))
         id_list = list(data_df['openid'])
         y = data_df['charge']
+        G = nx.read_gpickle('my_graph.gpickle')
         G = G.subgraph(id_list)
         feature = load_obj('feature')
         x = np.array([feature[i] for i in id_list])
@@ -40,99 +177,210 @@ else:
         val_mask[i] = 1
     for i in range(int(x.shape[0] * 0.9), x.shape[0]):
         test_mask[i] = 1
+    if os.path.exists('small_graph.gpickle'):
+        G_s = nx.read_gpickle('small_graph.gpickle')
+    else:
+        G = nx.read_gpickle('my_graph.gpickle')
+        G = G.subgraph(list(id_list))
+        ori_key = list(G.node.keys())
+        ori_key.sort()
+        projection = {}
+        for i in range(len(id_list)):
+            projection[ori_key[i]] = i
+        edge_index = G.edges()
+        src = [projection[i[0]] for i in edge_index]
+        dst = [projection[i[1]] for i in edge_index]
+        g = DGLGraph()
+        g.add_nodes(len(id_list))
+        g.add_edges(src, dst)
+        G_s = g.to_networkx()
+        nx.write_gpickle(G_s,'small_graph.gpickle')
+        # edge_index = [[projection[i[0]], projection[i[1]]] for i in edge_index]
+        # G_s = nx.DiGraph()
+        # G_s.add_edges_from(edge_index)
+        # nx.write_gpickle(G_s,'small_graph.gpickle')
+    # if args.self_loop and not args.dataset.startswith('reddit'):
+    #     data.graph.add_edges_from([(i,i) for i in range(len(data.graph))])
 
-    G = G.subgraph(list(id_list))
-    ori_key = list(G.node.keys())
-    ori_key.sort()
-    projection = {}
-    for i in range(len(id_list)):
-        projection[ori_key[i]] = i
-    edge_index = G.edges()
-    edge_index = [[projection[i[0]],projection[i[1]]] for i in edge_index]
-    # edge_index = []
-    edge_index = torch.tensor(edge_index).t().contiguous()
-    edge_index = edge_index - edge_index.min()
-    edge_index, _ = remove_self_loops(edge_index)
+    train_nid = np.nonzero(train_mask)[0].astype(np.int64)
+    test_nid = np.nonzero(test_mask)[0].astype(np.int64)
 
-    # x = RobustScaler().fit_transform(x)
-    x = torch.from_numpy(x).to(torch.float)
-    y = y.reshape(-1, 1)
-    # y = StandardScaler().fit_transform(y)           # transform
-    y = torch.from_numpy(y).to(torch.float)
-    train_mask =  torch.from_numpy(train_mask).to(torch.uint8)
-    val_mask =  torch.from_numpy(val_mask).to(torch.uint8)
-    test_mask =  torch.from_numpy(test_mask).to(torch.uint8)
+    features = torch.FloatTensor(x)
+    labels = torch.FloatTensor(y)
+    train_mask = torch.ByteTensor(train_mask)
+    val_mask = torch.ByteTensor(val_mask)
+    test_mask = torch.ByteTensor(test_mask)
+    in_feats = features.shape[1]
+    n_classes = 1
+    n_edges = G_s.number_of_edges()
 
-    data = Data(edge_index=edge_index, x=x, y=y, train_mask=train_mask, val_mask=val_mask,
-                test_mask=test_mask)
-    torch.save(data, 'data.npz')
-loader = NeighborSampler(data, size=[25, 10], num_hops=2, batch_size=1000,
-                         shuffle=True, add_self_loops=True)
+    n_train_samples = train_mask.sum().item()
+    n_val_samples = val_mask.sum().item()
+    n_test_samples = test_mask.sum().item()
 
+    print("""----Data statistics------'
+      #Edges %d
+      #Train samples %d
+      #Val samples %d
+      #Test samples %d""" %
+          (n_edges,
+              n_train_samples,
+              n_val_samples,
+              n_test_samples))
 
-class Net(torch.nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(Net, self).__init__()
-        self.conv1 = SAGEConv(in_channels, 32)
-        self.conv2 = SAGEConv(32, out_channels)
+    # create GCN model
+    # test = nx.DiGraph()
+    # test.add_edges_from([[0,1][1,2]])
+    g = DGLGraph(G_s,readonly=True)
+    # g.add_nodes(len(id_list))
+    # g.add_edges(src,dst)
+    # g = DGLGraph(g.to_networkx(),readonly=True )
+    norm = 1. / (g.in_degrees().float()+1e-3).unsqueeze(1)
 
+    if args.gpu < 0:
+        cuda = False
+    else:
+        cuda = True
+        torch.cuda.set_device(args.gpu)
+        features = features.cuda()
+        labels = labels.cuda()
+        train_mask = train_mask.cuda()
+        val_mask = val_mask.cuda()
+        test_mask = test_mask.cuda()
+        norm = norm.cuda()
 
+    g.ndata['features'] = features
 
-    def forward(self, x, data_flow):
-        data = data_flow[0]
-        x = x[data.n_id]
-        x = F.relu(self.conv1(x, data.edge_index,size=data.size))
-        x = F.dropout(x, training=self.training)
-        data = data_flow[1]
-        x = self.conv2(x, data.edge_index,size=data.size)
-        return x
+    num_neighbors = args.num_neighbors
+    n_layers = args.n_layers
 
-class GATNet(torch.nn.Module):
-    def __init__(self, in_channels, out_channels):
-        super(GATNet, self).__init__()
-        self.conv1 = GATConv(in_channels, 8, heads=8, dropout=0.6)
-        self.conv2 = GATConv(8 * 8, out_channels, heads=1, concat=True,
-                             dropout=0.6)
+    g.ndata['norm'] = norm
 
-    def forward(self, x, data_flow):
-        data = data_flow[0]
-        x = x[data.n_id]
-        x = F.elu(self.conv1((x, None), data.edge_index, data.size))
-        x = F.dropout(x, p=0.6, training=self.training)
-        data = data_flow[1]
-        x = self.conv2((x, None), data.edge_index, data.size)
-        return F.log_softmax(x, dim=1)
-device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
-model = GATNet(67, 1).to(device)
-optimizer = torch.optim.Adam(model.parameters(), lr=0.01, weight_decay=5e-4)
+    g.update_all(fn.copy_src(src='features', out='m'),
+                 fn.sum(msg='m', out='preprocess'),
+                 lambda node : {'preprocess': node.data['preprocess'] * node.data['norm']})
+    hh = g.ndata['preprocess']
+    for i in range(n_layers):
+        g.ndata['h_{}'.format(i)] = torch.zeros(features.shape[0], args.n_hidden).to(device=features.device)
 
-
-def train():
-    model.train()
-
-    total_loss = 0
-    for data_flow in loader(data.train_mask):
-        optimizer.zero_grad()
-        out = model(data.x.to(device), data_flow.to(device))
-        loss = F.mse_loss(out, data.y[data_flow.n_id].to(device))
-        loss.backward()
-        optimizer.step()
-        total_loss += loss.item() * data_flow.batch_size
-    return total_loss / data.train_mask.sum().item()
-
-
-def test(mask):
-    model.eval()
-
-    loss = 0
-    for data_flow in loader(mask):
-        pred = model(data.x.to(device), data_flow.to(device))
-        loss += F.mse_loss(pred, data.y[data_flow.n_id].to(device))* data_flow.batch_size
-    return loss / data.test_mask.sum().item()
+    g.ndata['h_{}'.format(n_layers-1)] = torch.zeros(features.shape[0], 2*args.n_hidden).to(device=features.device)
 
 
-for epoch in range(1, 1001):
-    loss = train()
-    test_acc = test(data.test_mask)
-    print('Epoch: {:02d}, Loss: {:.4f}, Test: {:.4f}'.format(
-        epoch, loss, test_acc))
+    model = GCNSampling(in_feats,
+                        args.n_hidden,
+                        n_classes,
+                        n_layers,
+                        F.relu,
+                        args.dropout)
+
+    loss_fcn = nn.MSELoss()
+
+    infer_model = GCNInfer(in_feats,
+                           args.n_hidden,
+                           n_classes,
+                           n_layers,
+                           F.relu)
+
+    if cuda:
+        model.cuda()
+        infer_model.cuda()
+
+    # use optimizer
+    optimizer = torch.optim.Adam(model.parameters(),
+                                 lr=args.lr,
+                                 weight_decay=args.weight_decay)
+
+    for epoch in range(args.n_epochs):
+        for nf in dgl.contrib.sampling.NeighborSampler(g, args.batch_size,
+                                                       num_neighbors,
+                                                       neighbor_type='in',
+                                                       shuffle=True,
+                                                       num_workers=32,
+                                                       num_hops=n_layers,
+                                                       seed_nodes=train_nid):
+            for i in range(n_layers):
+                agg_history_str = 'agg_h_{}'.format(i)
+                g.pull(nf.layer_parent_nid(i+1).long(), fn.copy_src(src='h_{}'.format(i), out='m'),
+                       fn.sum(msg='m', out=agg_history_str),
+                       lambda node : {agg_history_str: node.data[agg_history_str] * node.data['norm']})
+
+            node_embed_names = [['preprocess', 'h_0']]
+            for i in range(1, n_layers):
+                node_embed_names.append(['h_{}'.format(i), 'agg_h_{}'.format(i-1)])
+            node_embed_names.append(['agg_h_{}'.format(n_layers-1)])
+            nf.copy_from_parent(node_embed_names=node_embed_names)
+
+            model.train()
+            # forward
+            pred = model(nf)
+            batch_nids = nf.layer_parent_nid(-1).to(device=pred.device).long()
+            batch_labels = labels[batch_nids]
+            loss = loss_fcn(pred, batch_labels)
+
+            optimizer.zero_grad()
+            loss.backward()
+            optimizer.step()
+
+            node_embed_names = [['h_{}'.format(i)] for i in range(n_layers)]
+            node_embed_names.append([])
+            nf.copy_to_parent(node_embed_names=node_embed_names)
+
+
+        for infer_param, param in zip(infer_model.parameters(), model.parameters()):
+            infer_param.data.copy_(param.data)
+
+
+
+        for nf in dgl.contrib.sampling.NeighborSampler(g, args.test_batch_size,
+                                                       g.number_of_nodes(),
+                                                       neighbor_type='in',
+                                                       num_workers=32,
+                                                       num_hops=n_layers,
+                                                       seed_nodes=test_nid):
+            node_embed_names = [['preprocess']]
+            for i in range(n_layers):
+                node_embed_names.append(['norm'])
+            nf.copy_from_parent(node_embed_names=node_embed_names)
+
+            infer_model.eval()
+            with torch.no_grad():
+                pred = infer_model(nf)
+                batch_nids = nf.layer_parent_nid(-1).to(device=pred.device).long()
+                batch_labels = labels[batch_nids]
+                loss = loss_fcn(pred, batch_labels)
+
+        print("Test MSE loss {:.4f}". format(loss.item()))
+
+
+if __name__ == '__main__':
+    parser = argparse.ArgumentParser(description='GCN')
+    register_data_args(parser)
+    parser.add_argument("--dropout", type=float, default=0.5,
+            help="dropout probability")
+    parser.add_argument("--gpu", type=int, default=-1,
+            help="gpu")
+    parser.add_argument("--lr", type=float, default=3e-2,
+            help="learning rate")
+    parser.add_argument("--n-epochs", type=int, default=200,
+            help="number of training epochs")
+    parser.add_argument("--batch-size", type=int, default=100,
+            help="train batch size")
+    parser.add_argument("--test-batch-size", type=int, default=100,
+            help="test batch size")
+    parser.add_argument("--num-neighbors", type=int, default=2,
+            help="number of neighbors to be sampled")
+    parser.add_argument("--n-hidden", type=int, default=16,
+            help="number of hidden gcn units")
+    parser.add_argument("--n-layers", type=int, default=1,
+            help="number of hidden gcn layers")
+    parser.add_argument("--self-loop", action='store_true',
+            help="graph self-loop (default=False)")
+    parser.add_argument("--weight-decay", type=float, default=5e-4,
+            help="Weight for L2 loss")
+    args = parser.parse_args()
+
+    print(args)
+
+    main(args)
+
+
