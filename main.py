81c514060b2eddbee6fbb8005336dd0e12313e72
wei <w21180239@163.com>
2019-07-22 11:36:55 +0800

    add predict

--- main.py
+++ main.py
@@ -1,16 +1,17 @@
-import os
-
+import numpy as np
+import pandas as pd
 import torch
 import torch.nn.functional as F
-from sklearn.preprocessing import StandardScaler,RobustScaler,MaxAbsScaler
+from sklearn.preprocessing import RobustScaler
 from torch_geometric.data import DataLoader
 from torch_geometric.nn import GATConv
 
 from pytorchtools import EarlyStopping
 
 # os.environ["CUDA_VISIBLE_DEVICES"] = "2"
-batchsize = 6000
-torch.cuda.set_device(2)
+batchsize = 100
+torch.cuda.set_device(0)
+
 
 class Net(torch.nn.Module):
     def __init__(self, in_channels, out_channels):
@@ -29,28 +30,39 @@ class Net(torch.nn.Module):
 ea = EarlyStopping(verbose=True)
 train_data_list = torch.load('subway_data_train.npz')
 val_data_list = torch.load('subway_data_val.npz')
+pre_data_list = []
+pre_data_list.append(torch.load('subway_data_pre_15.npz'))
+pre_data_list.append(torch.load('subway_data_pre_30.npz'))
+pre_data_list.append(torch.load('subway_data_pre_45.npz'))
 x_scaler = RobustScaler()
-y_scaler = StandardScaler()
+# y_scaler = StandardScaler()
 y = torch.cat([data.y for data in train_data_list], 0)
 x = torch.cat([data.x for data in train_data_list], 0)
 x_scaler.fit(x)
-y_scaler.fit(y)
+# y_scaler.fit(y)
 for data in train_data_list:
     x = x_scaler.transform(data.x)
     data.x = torch.from_numpy(x).to(torch.float)
-    y = y_scaler.transform(data.y)
-    data.y = torch.from_numpy(y).to(torch.float)
+    # y = y_scaler.transform(data.y)
+    # data.y = torch.from_numpy(y).to(torch.float)
 for data in val_data_list:
     x = x_scaler.transform(data.x)
     data.x = torch.from_numpy(x).to(torch.float)
-    y = y_scaler.transform(data.y)
-    data.y = torch.from_numpy(y).to(torch.float)
+    # y = y_scaler.transform(data.y)
+    # data.y = torch.from_numpy(y).to(torch.float)
+for pre in pre_data_list:
+    for data in pre:
+        x = x_scaler.transform(data.x)
+        data.x = torch.from_numpy(x).to(torch.float)
+        # y = y_scaler.transform(data.y)
+        # data.y = torch.from_numpy(y).to(torch.float)
 train_loader = DataLoader(
     train_data_list, batch_size=batchsize, shuffle=True)
 
 val_loader = DataLoader(
     val_data_list, batch_size=batchsize, shuffle=True)
 
+pre_loader_list = [DataLoader(pre, batch_size=batchsize, shuffle=False) for pre in pre_data_list]
 device = torch.device('cuda' if torch.cuda.is_available() else 'cpu')
 model = Net(train_data_list[0].num_features, train_data_list[0].y.size(1)).to(device)
 optimizer = torch.optim.Adam(model.parameters(), lr=0.001, weight_decay=5e-4)
@@ -68,7 +80,8 @@ def train(loader):
         optimizer.step()
         loss_list = [F.mse_loss(out[:, i], data.y.to(device)[:, i]).item() for i in range(3)]
         total_loss.append(loss_list)
-    total_loss = y_scaler.inverse_transform(total_loss)
+    # total_loss = y_scaler.inverse_transform(total_loss)
+    total_loss = np.array(total_loss)
     total_loss = [(total_loss[:, i].sum() / len(total_loss)) ** 0.5 for i in range(3)]
     return total_loss
 
@@ -81,16 +94,37 @@ def test(loader):
         out = model(data.x.to(device), data.edge_index.to(device))
         loss_list = [F.mse_loss(out[:, i], data.y.to(device)[:, i]).item() for i in range(3)]
         total_loss.append(loss_list)
-    total_loss = y_scaler.inverse_transform(total_loss)
+    # total_loss = y_scaler.inverse_transform(total_loss)
+    total_loss = np.array(total_loss)
     total_loss = [(total_loss[:, i].sum() / len(total_loss)) ** 0.5 for i in range(3)]
     return total_loss
 
 
+def predict(loader_list):
+    model.eval()
+    result = []
+    for i in range(len(loader_list)):
+        tmp = []
+        for data in loader_list[i]:
+            out = model(data.x.to(device), data.edge_index.to(device))
+            # out = y_scaler.inverse_transform(out.cpu().detach().numpy())
+            tmp.append(out.cpu().detach().numpy()[:, i])
+        cat_tmp = np.concatenate(tmp, 0)
+        result.append(cat_tmp)
+    return result
+
+
+def write_out(result):
+    df = pd.DataFrame({'15min': result[0], '30min': result[1], '45min': result[2]})
+    df.to_csv('predict_out.csv', index=False, )
 for epoch in range(1, 10001):
     loss = train(train_loader)
     val_loss = test(val_loader)
-    # ea(sum(val_loss), model)
-    # if ea.early_stop:
-    #     print('early stop!')
-    #     break
+    ea(sum(val_loss), model)
+    if ea.early_stop:
+        print('early stop!')
+        break
     print(f'Epoch:{epoch}\nTrain:{loss[0]}\t{loss[1]}\t{loss[2]}\nTest:{val_loss[0]}\t{val_loss[1]}\t{val_loss[2]}')
+print('predicting...')
+re = predict(pre_loader_list)
+write_out(re)
--- preprocess.py
+++ preprocess.py
@@ -35,14 +35,28 @@ G = nx.read_gpickle('subway_g.gpickle')
 data_df = pd.DataFrame(pd.read_csv('Subway_instation_data_01.csv', header=None))
 data = data_df.values
 
-x_list = [data[i:i+windows,:] for i in range(data.shape[0]-windows+1)]
+x_list = [data[i:i + windows, :] for i in range(data.shape[0] - windows + 1 - 3 * 288)]
 y_list = [[x[windows - 7] for x in x_list], [x[windows - 4] for x in x_list], [x[windows - 1] for x in x_list]]
 y_list = [np.array([y_list[0][i], y_list[1][i], y_list[2][i]]) for i in range(len(y_list[0]))]
+pre_win = [15, 18, 21]
+pre_x_list = []
+pre_y_list = []
+for win in pre_win:
+    hh = [data[i:i + win, :] for i in range(len(data) - 3 * 288 - win + 1, data.shape[0] - win + 1)]
+    yy = [x[len(x) - 1, :] for x in hh]
+    hh = [x[:12] for x in hh]
+    pre_x_list.append(hh)
+    pre_y_list.append(yy)
+
 # y_list = np.array(y_list)
 # np.swapaxes(y_list, 1, 2)
 x_list = [x[:windows - 9] for x in x_list]
 x_list = [np.swapaxes(x, 0, 1) for x in x_list]
 y_list = [np.swapaxes(y, 0, 1) for y in y_list]
+
+for i in range(3):
+    pre_x_list[i] = [np.swapaxes(x, 0, 1) for x in pre_x_list[i]]
+    pre_y_list[i] = [np.reshape(y, (81, -1)) for y in pre_y_list[i]]
 
 randnum = random.randint(0, 100)
 random.seed(randnum)
@@ -74,6 +88,13 @@ for i, x in enumerate(x_list):
     x_list[i] = torch.from_numpy(x).to(torch.float)
 for i, y in enumerate(y_list):
     y_list[i] = torch.from_numpy(y).to(torch.float)
+
+for j in range(3):
+    for i, x in enumerate(pre_x_list[j]):
+        pre_x_list[j][i] = torch.from_numpy(x).to(torch.float)
+    for i, y in enumerate(pre_y_list[j]):
+        pre_y_list[j][i] = torch.from_numpy(y).to(torch.float)
+
 # train_mask =  torch.from_numpy(train_mask).to(torch.uint8)
 # val_mask =  torch.from_numpy(val_mask).to(torch.uint8)
 # test_mask =  torch.from_numpy(test_mask).to(torch.uint8)
@@ -81,6 +102,7 @@ for i, y in enumerate(y_list):
 
 train_list = []
 val_list = []
+pre_list = [[Data(edge_index=edge_index, x=x, y=y) for x, y in zip(pre_x_list[j], pre_y_list[j])] for j in range(3)]
 i = 0
 total = len(x_list)
 for x, y in zip(x_list, y_list):
@@ -90,5 +112,9 @@ for x, y in zip(x_list, y_list):
     else:
         train_list.append(data)
     i += 1
+
 torch.save(train_list, 'subway_data_train.npz')
 torch.save(val_list, 'subway_data_val.npz')
+torch.save(pre_list[0], 'subway_data_pre_15.npz')
+torch.save(pre_list[1], 'subway_data_pre_30.npz')
+torch.save(pre_list[2], 'subway_data_pre_45.npz')
